d5df2039fa8889e446ea8ef7b3a8fdd49c340f30
Leandro Adegas <ecommerce.adegas@gmail.com>
2023-09-27 15:08:19 -0300

    in

--- Projetos/sisbancario/conta.py
+++ Projetos/sisbancario/conta.py
@@ -1,28 +1,64 @@
 class Conta:
     def __init__(self, numero:int, saldo:float):
+        self.numero = numero
         self.limite = 100
-        self.saldo = self.limite
+        self.saldo = saldo + self.limite
         self.extrato = []
+
+
 
     def getNumero(self):
         return self.numero
 
+
+
     def getSaldo(self):
         return self.saldo
 
+
+
     def getLimite(self):
-        return
+        return self.limite
+
+
 
     def sacar(self, valor:float):
-        self.saldo -= valor
-        print(f'- R${valor}')
+        if valor > self.saldo - self.limite:
+            self.limite = self.saldo - valor
+        if 0 < valor <= self.saldo:
+            self.saldo -= valor
+            self.extrato.append(-valor)
+            return True
+        else:
+            return False
+
+
 
     def depositar(self, valor:float):
-        self.saldo += valor
-        print(f'+ R${valor}')
+        if valor > 0:
+            self.saldo += valor
+            self.extrato.append(valor)
+            if self.limite == 0:
+                self.limite += valor
+            if self.saldo >= 100:
+                self.limite = 100
+            return True
+        else:
+            return False
+
+
 
     def transferir(self, destino, valor:float):
-        return False
+        if destino:
+            if 0 < valor <= self.saldo:
+                self.sacar(valor)
+                destino.depositar(valor)
+                print('Transação realizada com sucesso!')
+                return True
+            else:
+                return False
+
+
 
     def verExtrato(self):
         return self.extrato
