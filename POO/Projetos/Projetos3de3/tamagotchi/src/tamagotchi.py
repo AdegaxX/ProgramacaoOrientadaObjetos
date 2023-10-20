0c34e2a74b02117157f5c2fa37df26e638809a6e
Leandro Adegas <ecommerce.adegas@gmail.com>
2023-10-20 18:51:10 -0300

    in

--- POO/Projetos/Projetos3de3/tamagotchi/src/tamagotchi.py
+++ POO/Projetos/Projetos3de3/tamagotchi/src/tamagotchi.py
@@ -1,46 +1,69 @@
 class Tamagotchi:
 
     def __init__(self, energiaMax:int, saciedadeMax:int, limpezaMax:int, idadeMax:int ):
-        pass
+        self.energiaMax = energiaMax
+        self.saciedadeMax = saciedadeMax
+        self.limpezaMax = limpezaMax
+        self.idadeMax = idadeMax
+        self.energiaAtual = energiaMax
+        self.saciedadeAtual = saciedadeMax
+        self.limpezaAtual = limpezaMax
+        self.idadeAtual = 0
+        self.diamantes = 0
+        self.EstaVivo = True
+
 
     def getEnergiaMax(self):
-        return -1
+        return self.energiaMax
 
     def getSaciedadeMax(self):
-        return -1
+        return self.saciedadeMax
 
     def getLimpezaMax(self):
-        return -1
+        return self.limpezaMax
 
     def getIdadeMax(self):
-        return -1
+        return self.idadeMax
 
     def getEnergiaAtual(self):
-        return -1
+        return self.energiaAtual
 
     def getSaciedadeAtual(self):
-        return -1
+        return self.saciedadeAtual
 
     def getLimpezaAtual(self):
-        return -1
+        return self.saciedadeAtual
 
     def getIdadeAtual(self):
-        return -1
+        return self.idadeAtual
 
     def getDiamantes(self):
-        return -1
+        return self.diamantes
 
     def getEstaVivo(self):
         return True
 
     def brincar(self):
+        if not self.EstaVivo:
+            return False
+        self.energiaAtual -= 2
+        self.saciedadeAtual -= 1
+        self.limpezaAtual -= 3
+        self.diamantes += 1
+        self.idadeAtual += 1
         return True
 
     def comer(self):
+        if not self.EstaVivo:
+            return False
         return True
 
     def dormir(self):
+        if not self.EstaVivo:
+            return False
         return True
 
     def banhar(self):
+        if not self.EstaVivo:
+            return False
         return True
